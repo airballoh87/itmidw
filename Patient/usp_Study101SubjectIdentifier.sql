bf92bfcb3eb75021e119019879a59119fd9ecd17
airballoh87 <aaronblack12@gmail.com>
2014-06-09 22:36:28 -0400

    comments and organization of code

--- Patient/usp_Study101SubjectIdentifier.sql
+++ Patient/usp_Study101SubjectIdentifier.sql
@@ -2,13 +2,13 @@ IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'itmidw.[usp_S
 DROP PROCEDURE itmidw.[usp_Study101SubjectIdentifier]
 GO	
 /**************************************************************************
-Created On : 4/1/2014
-Created By : Aaron Black
+Created ON : 4/1/2014
+Created By : AarON Black
 Team Name : Informatics
 Object name : [usp_Study101SubjectIdentifier]
-Functional : ITMI SSIS for Insert and Update for study 101 subject Identifier
-Purpose : Import of study 101, this shoudld be for MRN and other idnetifiers that need isolated
-History : Created on 4/1/2014
+Functional : ITMI SSIS for Insert AND Update for study 101 subject Identifier
+Purpose : Import of study 101, this shoudld be for MRN AND other idnetifiers that need isolated
+History : Created ON 4/1/2014
 *************************************************************************
 Date Modified By QC# Purposes
 **************************************************************************
@@ -16,14 +16,14 @@ Date Modified By QC# Purposes
 **************************************************************************
 USE CASE:
 EXEC [usp_Study101SubjectIdentifier]
---testing update and delete
+--testing update AND delete
 **************************************************************************/
 CREATE PROCEDURE itmidw.[usp_Study101SubjectIdentifier]
 AS
 BEGIN
 SET NOCOUNT ON;
-DECLARE @UpdatedOn SMALLDATETIME
-SET @UpdatedOn = CAST(GETDATE() AS SMALLDATETIME)
+DECLARE @UpdatedON SMALLDATETIME
+SET @UpdatedON = CAST(GETDATE() AS SMALLDATETIME)
 PRINT CONVERT(CHAR(23), @UpdatedOn, 121) + ' [usp_Study101SubjectIdentifier][' + @@SERVERNAME + '][' + SYSTEM_USER + ']'
 PRINT 'INSERT [ITMIDW].itmidw.[usp_Study101SubjectIdentifier]...'
 
@@ -37,104 +37,118 @@ SELECT sub.SubjectID, sourceSystemIDLabel,'itmi SubjectID', 6, GETDATE(), 'usp_S
 FROM itmidw.tblSubject sub 
 	LEFT JOIN itmidw.tblSubjectIdentifer id
 		ON id.subjectID = sub.subjectID
-			and id.subjectIdentifier = sub.sourceSystemIDLabel
+			AND id.subjectIdentifier = sub.sourceSystemIDLabel
 WHERE sub.orgSourceSystemID = (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'INFOPATH') 
 	AND id.subjectIdentiferID IS NULL
 
---MRNs from extract
---Mom
+--*************************************
+--*******--MRNs FROM extract***********
+--*************************************
+
+
+--*************************************
+--*******--mom*************************
+--*************************************
 INSERT INTO itmidw.[tblSubjectIdentifer]([subjectID],[subjectIdentifier],[subjectIdentifierType],[orgSourceSystemID],[createDate],[createdBy])
 SELECT 
 	sub.subjectID
 	, m.[mom MRN]
 	, 'MRN'
-	, (select sourceSystemID from itmidw.tblSourceSystem where sourceSystemShortName = 	'ITMIClinical') 
+	, (SELECT sourceSystemID FROM itmidw.tblSourceSystem WHERE sourceSystemShortName = 	'ITMIClinical') 
 	, GETDATE()
 	, 'usp_Study101SubjectIdentifier'
 FROM epic.epicMrn m
 	INNER JOIN itmidw.tblSubject sub
-		on sub.sourceSystemIDLabel = 'M-101-' + m.[study id]
+		ON sub.sourceSystemIDLabel = 'M-101-' + m.[study id]
 	LEFT JOIN itmidw.tblSubjectIdentifer id
-		on id.subjectIdentifier = m.[mom MRN]
-			and sub.subjectID = id.subjectID
+		ON id.subjectIdentifier = m.[mom MRN]
+			AND sub.subjectID = id.subjectID
 WHERE m.[mom mrn] IS NOT NULL
-	and id.subjectIdentiferID IS NULL
+	AND id.subjectIdentiferID IS NULL
 
 	
---Baby 
+--*************************************
+--*******baby**************************
+--*************************************
 INSERT INTO itmidw.[tblSubjectIdentifer]([subjectID],[subjectIdentifier],[subjectIdentifierType],[orgSourceSystemID],[createDate],[createdBy])
 SELECT 
 	sub.subjectID
 	, m.[nb MRN]
 	, 'MRN'
-	, (select sourceSystemID from itmidw.tblSourceSystem where sourceSystemShortName = 	'ITMIClinical') 
+	, (SELECT sourceSystemID FROM itmidw.tblSourceSystem WHERE sourceSystemShortName = 	'ITMIClinical') 
 	, GETDATE()
 	, 'usp_Study101SubjectIdentifier'
 FROM epic.epicMrn m
 	INNER JOIN itmidw.tblSubject sub
-		on sub.sourceSystemIDLabel = 'NB-101-' + m.[study id]
+		ON sub.sourceSystemIDLabel = 'NB-101-' + m.[study id]
 	LEFT JOIN itmidw.tblSubjectIdentifer id
-		on id.subjectIdentifier = m.[nb MRN]
-			and sub.subjectID = id.subjectID
+		ON id.subjectIdentifier = m.[nb MRN]
+			AND sub.subjectID = id.subjectID
 WHERE m.[nb mrn] IS NOT NULL
-	and id.subjectIdentiferID IS NULL
+	AND id.subjectIdentiferID IS NULL
 
---baby dups 'A'
+--*************************************
+--*******--baby dups a*****************
+--*************************************
+
 INSERT INTO itmidw.[tblSubjectIdentifer]([subjectID],[subjectIdentifier],[subjectIdentifierType],[orgSourceSystemID],[createDate],[createdBy])
 SELECT 
 	sub.subjectID
 	, m.[nb MRN]
 	, 'MRN'
-	, (select sourceSystemID from itmidw.tblSourceSystem where sourceSystemShortName = 	'ITMIClinical') 
+	, (SELECT sourceSystemID FROM itmidw.tblSourceSystem WHERE sourceSystemShortName = 	'ITMIClinical') 
 	, GETDATE()
 	, 'usp_Study101SubjectIdentifier'
 FROM epic.epicMrn m
 	INNER JOIN itmidw.tblSubject sub
 		ON sub.sourceSystemIDLabel = 'NB-A-101-' + m.[study id]
 	LEFT JOIN itmidw.tblSubjectIdentifer id
-		on id.subjectIdentifier = m.[nb MRN]
-			and sub.subjectID = id.subjectID
+		ON id.subjectIdentifier = m.[nb MRN]
+			AND sub.subjectID = id.subjectID
 WHERE m.[nb mrn] IS NOT NULL
-	and id.subjectIdentiferID IS NULL
+	AND id.subjectIdentiferID IS NULL
 
---baby dups b
+--*************************************
+--*******--baby dups b*****************
+--*************************************
 INSERT INTO itmidw.[tblSubjectIdentifer]([subjectID],[subjectIdentifier],[subjectIdentifierType],[orgSourceSystemID],[createDate],[createdBy])
 SELECT 
 	sub.subjectID
 	, m.[nbmrn2]
 	, 'MRN'
-	, (select sourceSystemID from itmidw.tblSourceSystem where sourceSystemShortName = 	'ITMIClinical') 
+	, (SELECT sourceSystemID FROM itmidw.tblSourceSystem WHERE sourceSystemShortName = 	'ITMIClinical') 
 	, GETDATE()
 	, 'usp_Study101SubjectIdentifier'
 FROM epic.epicMrn m
 	INNER JOIN itmidw.tblSubject sub
 		ON sub.sourceSystemIDLabel = 'NB-B-101-' + m.[study id]
 	LEFT JOIN itmidw.tblSubjectIdentifer id
-		on id.subjectIdentifier = m.[nbmrn2]
-			and sub.subjectID = id.subjectID
+		ON id.subjectIdentifier = m.[nbmrn2]
+			AND sub.subjectID = id.subjectID
 WHERE m.nbmrn2 IS NOT NULL
-	and id.subjectIdentiferID IS NULL
+	AND id.subjectIdentiferID IS NULL
 
 
 
-
---Father
+--*************************************
+--*******father************************
+--*************************************
 INSERT INTO itmidw.[tblSubjectIdentifer]([subjectID],[subjectIdentifier],[subjectIdentifierType],[orgSourceSystemID],[createDate],[createdBy])
 SELECT 
 	sub.subjectID
 	, m.[father MRN]
 	, 'MRN'
-	, (select sourceSystemID from itmidw.tblSourceSystem where sourceSystemShortName = 	'ITMIClinical') 
+	, (SELECT sourceSystemID FROM itmidw.tblSourceSystem WHERE sourceSystemShortName = 	'ITMIClinical') 
 	, GETDATE()
 	, 'usp_Study101SubjectIdentifier'
 FROM epic.epicMrn m
 	INNER JOIN itmidw.tblSubject sub
-		on sub.sourceSystemIDLabel = 'F-101-' + m.[study id]
+		ON sub.sourceSystemIDLabel = 'F-101-' + m.[study id]
 	LEFT JOIN itmidw.tblSubjectIdentifer id
-		on id.subjectIdentifier = m.[father MRN]
-			and sub.subjectID = id.subjectID
+		ON id.subjectIdentifier = m.[father MRN]
+			AND sub.subjectID = id.subjectID
 WHERE m.[father MRN] IS NOT NULL
-	and id.subjectIdentiferID IS NULL
+	AND id.subjectIdentiferID IS NULL
 
 
 PRINT CAST(@@ROWCOUNT AS VARCHAR(10))+ ' row(s) updated.'
