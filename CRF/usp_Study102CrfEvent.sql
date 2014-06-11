a7089103f104f3119d54974cf8fb5d27c946b504
airballoh87 <aaronblack12@gmail.com>
2014-06-11 00:01:28 -0400

    update to comments

--- CRF/usp_Study102CrfEvent.sql
+++ CRF/usp_Study102CrfEvent.sql
@@ -6,7 +6,7 @@ Created On : 3/29/2014
 Created By : Aaron Black
 Team Name : Informatics
 Object name : [usp_Study102CrfEvent]
-Functional : ITMI SSIS for Insert and Update for study 102 tblCrfEvent
+Functional : ITMI SSIS for Insert AND Update for study 102 tblCrfEvent
  Which is attaching subjects to other entities, including family, original site WHERE the subject was enrolled.
 History : Created on 3/29/2014
 **************************************************************************
@@ -30,28 +30,30 @@ PRINT CONVERT(CHAR(23), @UpdatedOn, 121) + ' [usp_Study102CrfEvent][' + @@SERVER
 PRINT 'INSERT [ITMIDW].[dbo].[usp_Study102CrfEvent]...'
 
 --*************************************
---******************102****************
+--***************--drop table**********
 --*************************************
---drop table
+
 IF OBJECT_ID('tempdb..#sourceCrfEvent') IS NOT NULL
 DROP TABLE #sourceCrfEvent
 
-
+--*************************************
+--********insert into Temp table*******
+--*************************************
 SELECT DISTINCT
-           CONVERT(Varchar(100),deets.recordID) AS [sourceSystemCrfEventID]
-           ,NULL AS [parentCrfEventID]
-           ,crf.crfID AS [crfID]
-           ,deets.itmiFormName AS [crfType]
-           ,crf.crfID AS [crfVersionID] --at this time the ID' are the same **Change control** if now versions of the forms need to be tracked.
-           ,subject.subjectID AS [subjectID]
-           ,NULL AS [crfStatus]
-           ,MAX(dbo.dateonly(RaveCreatedDateTime)) AS  [updatedDate]
-           ,MIN(dbo.dateonly(revieweddatetime))  AS [completedDate]
-           , (SELECT studyID FROM itmidw.tblstudy WHERE [studyShortID] like '%102%') AS StudyId
-           ,NULL AS [eventCrfUUID]
-           ,(SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
-           ,GETDATE() [createDate]
-           ,'usp_Study102CrfEvent' AS [createdBy]
+   CONVERT(Varchar(100),deets.recordID) AS [sourceSystemCrfEventID]
+   ,NULL AS [parentCrfEventID]
+   ,crf.crfID AS [crfID]
+   ,deets.itmiFormName AS [crfType]
+   ,crf.crfID AS [crfVersionID] --at this time the ID' are the same **Change control** if now versions of the forms need to be tracked.
+   ,subject.subjectID AS [subjectID]
+   ,NULL AS [crfStatus]
+   ,MAX(dbo.dateonly(RaveCreatedDateTime)) AS  [updatedDate]
+   ,MIN(dbo.dateonly(revieweddatetime))  AS [completedDate]
+   ,(SELECT studyID FROM itmidw.tblstudy WHERE [studyShortID] like '%102%') AS StudyId
+   ,NULL AS [eventCrfUUID]
+   ,(SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
+   ,GETDATE() [createDate]
+   ,'usp_Study102CrfEvent' AS [createdBy]
 INTO #sourceCrfEvent
 FROM difzDBCopy.PatientDataPointDetail AS deets
 	INNER JOIN itmidw.tblSubject subject
@@ -59,241 +61,178 @@ FROM difzDBCopy.PatientDataPointDetail AS deets
 	INNER JOIN itmidw.tblCrf crf
 		ON crf.crfName = deets.itmiFormName
 WHERE deets.isactive = 1	
-	and subject.studyID = (SELECT studyID FROM itmidw.tblstudy WHERE [studyShortID] like '%102%')
+	AND subject.studyID = (SELECT studyID FROM itmidw.tblstudy WHERE [studyShortID] like '%102%')
 GROUP BY 
       deets.recordID
       ,crf.crfID 
       ,deets.itmiFormName
       ,subject.subjectID
 
+--*************************************
+--Surveys--****************************
+--*************************************
 
---Surveys
---baby6
+--*************************************
+--baby6--******************************
+--*************************************
 INSERT INTO #sourceCrfEvent ([sourceSystemCrfEventID], [parentCrfEventID], [crfID], [crfType], [crfVersionID], [subjectID], [crfStatus], [updatedDate], [completedDate], [StudyId], [eventCrfUUID], [orgSourceSystemID], [createDate], [createdBy])
 SELECT DISTINCT
-           --[sourceSystemCrfEventID]
-		ss.[family ID]
-           --, [parentCrfEventID]
-		, NULL
-           --, [crfID]
-		, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 6 Month Survery for Baby :102')
-           --, [crfType]
-		, 'Survey'
-           --, [crfVersionID]
-		,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
-           --, [subjectID]
-		, sub.subjectID
-           --, [crfStatus]
-		, NULL-- ss.[survey status]
-           --, [updatedDate]
-		, ss.[date completed]
-           --, [completedDate]
-		, ss.[date completed]
-           --, [StudyId]
-		, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
-           --, [eventCrfUUID]
-		, NULL
-           --, [orgSourceSystemID]
-        , (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
-		, GETDATE() [createDate]
-		, 'usp_Study102CrfEvent' AS [createdBy]
---SELECT *
+	ss.[family ID]
+	, NULL
+	, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 6 Month Survery for Baby :102')
+	, 'Survey'
+	,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
+	, sub.subjectID
+	, NULL-- ss.[survey status]
+	, ss.[date completed]
+	, ss.[date completed]
+	, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
+	, NULL
+	, (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
+	, GETDATE() [createDate]
+	, 'usp_Study102CrfEvent' AS [createdBy]
 FROM etl.SurveyBaby6_Stage ss
 	INNER JOIN itmidw.tblsubject sub
 		on sub.sourceSystemIDLabel = ss.[family ID]
 
---mom6
+--*************************************
+--mom6--*******************************
+--*************************************
 INSERT INTO #sourceCrfEvent ([sourceSystemCrfEventID], [parentCrfEventID], [crfID], [crfType], [crfVersionID], [subjectID], [crfStatus], [updatedDate], [completedDate], [StudyId], [eventCrfUUID], [orgSourceSystemID], [createDate], [createdBy])
 SELECT DISTINCT
-           --[sourceSystemCrfEventID]
-		ss.[family ID]
-           --, [parentCrfEventID]
-		, NULL
-           --, [crfID]
-		, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 6 Month Survery for Mom :102')
-           --, [crfType]
-		, 'Survey'
-           --, [crfVersionID]
-		,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
-           --, [subjectID]
-		, sub.subjectID
-           --, [crfStatus]
-		, NULL-- ss.[survey status]
-           --, [updatedDate]
-		, ss.[date completed]
-           --, [completedDate]
-		, ss.[date completed]
-           --, [StudyId]
-		, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
-           --, [eventCrfUUID]
-		, NULL
-           --, [orgSourceSystemID]
-        , (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
-		, GETDATE() [createDate]
-		, 'usp_Study102CrfEvent' AS [createdBy]
---SELECT *
+	ss.[family ID]
+	, NULL
+	, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 6 Month Survery for Mom :102')
+	, 'Survey'
+	,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
+	, sub.subjectID
+	, NULL-- ss.[survey status]
+	, ss.[date completed]
+	, ss.[date completed]
+	, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
+	, NULL 
+	, (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
+	, GETDATE() [createDate]
+	, 'usp_Study102CrfEvent' AS [createdBy]
 FROM etl.SurveyMom6_Stage ss
 	INNER JOIN itmidw.tblsubject sub
 		on sub.sourceSystemIDLabel = ss.[family ID]
 
---baby12
+--*************************************
+--baby12--*****************************
+--*************************************
 INSERT INTO #sourceCrfEvent ([sourceSystemCrfEventID], [parentCrfEventID], [crfID], [crfType], [crfVersionID], [subjectID], [crfStatus], [updatedDate], [completedDate], [StudyId], [eventCrfUUID], [orgSourceSystemID], [createDate], [createdBy])
 SELECT DISTINCT
-           --[sourceSystemCrfEventID]
-		ss.[family ID]
-           --, [parentCrfEventID]
-		, NULL
-           --, [crfID]
-		, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 12 Month Survery for Baby :102')
-           --, [crfType]
-		, 'Survey'
-           --, [crfVersionID]
-		,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
-           --, [subjectID]
-		, sub.subjectID
-           --, [crfStatus]
-		, NULL-- ss.[survey status]
-           --, [updatedDate]
-		, ss.[date completed]
-           --, [completedDate]
-		, ss.[date completed]
-           --, [StudyId]
-		, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
-           --, [eventCrfUUID]
-		, NULL
-           --, [orgSourceSystemID]
-        , (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
-		, GETDATE() [createDate]
-		, 'usp_Study102CrfEvent' AS [createdBy]
---SELECT *
+	ss.[family ID]
+	, NULL
+	, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 12 Month Survery for Baby :102')
+	, 'Survey'
+	,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
+	, sub.subjectID
+	, NULL-- ss.[survey status]
+	, ss.[date completed]
+	, ss.[date completed]
+	, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
+	, NULL
+	, (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
+	, GETDATE() [createDate]
+	, 'usp_Study102CrfEvent' AS [createdBy]
 FROM etl.SurveyBaby12_Stage ss
 	INNER JOIN itmidw.tblsubject sub
 		on sub.sourceSystemIDLabel = ss.[family ID]
 
---mom12
+--*************************************
+--mom12--******************************
+--*************************************
 INSERT INTO #sourceCrfEvent ([sourceSystemCrfEventID], [parentCrfEventID], [crfID], [crfType], [crfVersionID], [subjectID], [crfStatus], [updatedDate], [completedDate], [StudyId], [eventCrfUUID], [orgSourceSystemID], [createDate], [createdBy])
 SELECT DISTINCT
-           --[sourceSystemCrfEventID]
-		ss.[family ID]
-           --, [parentCrfEventID]
-		, NULL
-           --, [crfID]
-		, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 12 Month Survery for Mom :102')
-           --, [crfType]
-		, 'Survey'
-           --, [crfVersionID]
-		,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
-           --, [subjectID]
-		, sub.subjectID
-           --, [crfStatus]
-		, NULL-- ss.[survey status]
-           --, [updatedDate]
-		, ss.[date completed]
-           --, [completedDate]
-		, ss.[date completed]
-           --, [StudyId]
-		, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
-           --, [eventCrfUUID]
-		, NULL
-           --, [orgSourceSystemID]
-        , (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
-		, GETDATE() [createDate]
-		, 'usp_Study102CrfEvent' AS [createdBy]
---SELECT *
+	ss.[family ID]
+	, NULL
+	, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 12 Month Survery for Mom :102')
+	, 'Survey'
+	,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
+	, sub.subjectID
+	, NULL-- ss.[survey status]
+	, ss.[date completed]
+	, ss.[date completed]
+	, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
+	, NULL
+	, (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
+	, GETDATE() [createDate]
+	, 'usp_Study102CrfEvent' AS [createdBy]
 FROM etl.SurveyMom12_Stage ss
 	INNER JOIN itmidw.tblsubject sub
 		on sub.sourceSystemIDLabel = ss.[family ID]
 
---baby18
+--*************************************
+--baby18--*****************************
+--*************************************
 INSERT INTO #sourceCrfEvent ([sourceSystemCrfEventID], [parentCrfEventID], [crfID], [crfType], [crfVersionID], [subjectID], [crfStatus], [updatedDate], [completedDate], [StudyId], [eventCrfUUID], [orgSourceSystemID], [createDate], [createdBy])
 SELECT DISTINCT
-           --[sourceSystemCrfEventID]
-		ss.[family ID]
-           --, [parentCrfEventID]
-		, NULL
-           --, [crfID]
-		, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 18 Month Survery for Baby :102')
-           --, [crfType]
-		, 'Survey'
-           --, [crfVersionID]
-		,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
-           --, [subjectID]
-		, sub.subjectID
-           --, [crfStatus]
-		, NULL-- ss.[survey status]
-           --, [updatedDate]
-		, ss.[date completed]
-           --, [completedDate]
-		, ss.[date completed]
-           --, [StudyId]
-		, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
-           --, [eventCrfUUID]
-		, NULL
-           --, [orgSourceSystemID]
-        , (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
-		, GETDATE() [createDate]
-		, 'usp_Study102CrfEvent' AS [createdBy]
---SELECT *
+	ss.[family ID]
+	, NULL
+	, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 18 Month Survery for Baby :102')
+	, 'Survey'
+	,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
+	, sub.subjectID
+	, NULL-- ss.[survey status]
+	, ss.[date completed]
+	, ss.[date completed]
+	, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
+	, NULL
+	, (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
+	, GETDATE() [createDate]
+	, 'usp_Study102CrfEvent' AS [createdBy]
 FROM etl.SurveyBaby18_Stage ss
 	INNER JOIN itmidw.tblsubject sub
 		on sub.sourceSystemIDLabel = ss.[family ID]
 
 
+--*************************************
+--*************MOM 18******************
+--*************************************
 INSERT INTO #sourceCrfEvent ([sourceSystemCrfEventID], [parentCrfEventID], [crfID], [crfType], [crfVersionID], [subjectID], [crfStatus], [updatedDate], [completedDate], [StudyId], [eventCrfUUID], [orgSourceSystemID], [createDate], [createdBy])
 SELECT DISTINCT
-           --[sourceSystemCrfEventID]
-		ss.[family ID]
-           --, [parentCrfEventID]
-		, NULL
-           --, [crfID]
-		, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 18 Month Survery for Mom :102')
-           --, [crfType]
-		, 'Survey'
-           --, [crfVersionID]
-		,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
-           --, [subjectID]
-		, sub.subjectID
-           --, [crfStatus]
-		, NULL-- ss.[survey status]
-           --, [updatedDate]
-		, ss.[date completed]
-           --, [completedDate]
-		, ss.[date completed]
-           --, [StudyId]
-		, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
-           --, [eventCrfUUID]
-		, NULL
-           --, [orgSourceSystemID]
-        , (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
-		, GETDATE() [createDate]
-		, 'usp_Study102CrfEvent' AS [createdBy]
---SELECT *
+	ss.[family ID]
+	, NULL
+	, (Select crfID from itmidw.tblCrf crf where crf.crfName = 'Rave: 18 Month Survery for Mom :102')
+	, 'Survey'
+	,(SELECT v.crfVersionID from itmidw.tblCrfVersion v where v.crfVersionName = '6MonthBaby: v1')
+	, sub.subjectID
+	, NULL-- ss.[survey status]
+	, ss.[date completed]
+	, ss.[date completed]
+	, (select study.studyID FROM itmidw.tblstudy study where study.studyShortID = '102')
+	, NULL
+	, (SELECT ss.sourceSystemID FROM itmidw.tblSourceSystem ss WHERE ss.sourceSystemSHortName = 'DIFZ') AS [orgSourceSystemID]
+	, GETDATE() [createDate]
+	, 'usp_Study102CrfEvent' AS [createdBy]
 FROM etl.SurveyMom18_Stage ss
 	INNER JOIN itmidw.tblsubject sub
 		on sub.sourceSystemIDLabel = ss.[family ID]
 
 
-
---Slowly changing dimension
+--*************************************
+--Slowly changing dimension--**********
+--*************************************
 MERGE  ITMIDW.[tblCrfEvent] AS targetCrfEvent
 USING #sourceCrfEvent ss
 	ON targetCrfEvent.[sourceSystemCrfEventID] = ss.[sourceSystemCrfEventID]
-		and targetCrfEvent.[orgSourceSystemID] = ss.[orgSourceSystemID]
-		and targetCrfEvent.[crfId] = ss.[crfId]
+		AND targetCrfEvent.[orgSourceSystemID] = ss.[orgSourceSystemID]
+		AND targetCrfEvent.[crfId] = ss.[crfId]
   WHEN MATCHED
 	AND (
-           ss.[crfID] <>  targetCrfEvent.[crfID] OR
-           ss.[crfType] <> targetCrfEvent.[crfType] OR 
-           ss.[crfVersionID] <>  targetCrfEvent.[crfVersionID] OR 
-           ss.[subjectID] <> targetCrfEvent.[subjectID] OR 
-           ss.[crfStatus] <> targetCrfEvent.[crfStatus] OR 
-           ss.[updatedDate] <> targetCrfEvent.[updatedDate] OR 
-           ss.[completedDate] <> targetCrfEvent.[completedDate] OR 
-           ss.[studyID] <> targetCrfEvent.[studyID] OR 
-           ss.[eventCrfUUID] <> targetCrfEvent.[eventCrfUUID] OR 
-		 ss.[orgSourceSystemID] <> targetCrfEvent.[orgSourceSystemID] OR  
-		ss.[createDate] <> targetCrfEvent.[createDate] OR
-		ss.[createdBy] <> targetCrfEvent.[createdBy] 
+	   ss.[crfID] <>  targetCrfEvent.[crfID] OR
+	   ss.[crfType] <> targetCrfEvent.[crfType] OR 
+	   ss.[crfVersionID] <>  targetCrfEvent.[crfVersionID] OR 
+	   ss.[subjectID] <> targetCrfEvent.[subjectID] OR 
+	   ss.[crfStatus] <> targetCrfEvent.[crfStatus] OR 
+	   ss.[updatedDate] <> targetCrfEvent.[updatedDate] OR 
+	   ss.[completedDate] <> targetCrfEvent.[completedDate] OR 
+	   ss.[studyID] <> targetCrfEvent.[studyID] OR 
+	   ss.[eventCrfUUID] <> targetCrfEvent.[eventCrfUUID] OR 
+	   ss.[createDate] <> targetCrfEvent.[createDate] OR
+	   ss.[createdBy] <> targetCrfEvent.[createdBy] 
 	)
 THEN UPDATE SET
            [crfID]  = ss.[crfID]
